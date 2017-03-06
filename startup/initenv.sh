8cd441b18f6c2fe2b6057eefc4e57f1bc8837632
EAIBOT <eaibot@163.com>
2017-03-06 17:11:39 +0800

    update to 2.1.0

--- startup/initenv.sh
+++ startup/initenv.sh
@@ -9,5 +9,7 @@ echo  'KERNEL=="ttyACM*", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="5740", MOD
 
 echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", MODE:="0666", GROUP:="dialout",  SYMLINK+="ttyS2"' >/etc/udev/rules.d/imu.rules
 
-
+service udev reload
+sleep 2
+service udev restart
 
