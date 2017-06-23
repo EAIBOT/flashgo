d95b59b6b1cf95fad8f9013e7a952a21cbb49b08
EAIBOT <eaibot@163.com>
2017-06-23 15:07:43 +0800

    update to 2.3.0

--- src/flashgo_node.cpp
+++ src/flashgo_node.cpp
@@ -26,123 +26,88 @@
 
 Flashgo * drv = NULL;
 
-void publish_scan(ros::Publisher *pub, 
-                  node_info *nodes, 
-                  size_t node_count, ros::Time start,
-                  double scan_time,
-                  float angle_min, float angle_max, 
-                  std::string frame_id, std::vector<int> ignore_array , double ignore_value)
+void publish_scan(ros::Publisher *pub,  node_info *nodes,  size_t node_count, ros::Time start, double scan_time, float angle_min, float angle_max, std::string frame_id)
 {
-    static int scan_count = 0;
     sensor_msgs::LaserScan scan_msg;
+
+    float nodes_array[node_count];
+    for (size_t i = 0; i < node_count; i++) {
+        if(i<node_count/2){
+            nodes_array[node_count/2-1-i] = (float)nodes[i].distance_q2/4.0f/1000;
+        }else{
+            nodes_array[node_count-1-(i-node_count/2)] = (float)nodes[i].distance_q2/4.0f/1000;
+        }
+    }
+
+    int counts = node_count*((angle_max-angle_min)/360.0f);
+    int angle_start = 180+angle_min;
+    int node_start = node_count*(angle_start/360.0f);
+
+    scan_msg.ranges.resize(counts);
+    for (size_t i = 0; i < counts; i++) {
+        scan_msg.ranges[i] = nodes_array[i+node_start];
+    }
 
     scan_msg.header.stamp = start;
     scan_msg.header.frame_id = frame_id;
-    scan_count++;
-
-    scan_msg.angle_min = angle_min;
-    scan_msg.angle_max = angle_max;
-    scan_msg.angle_increment = (scan_msg.angle_max - scan_msg.angle_min) / (double)(node_count-1);
-
+    float radian_min = DEG2RAD(angle_min);
+    float radian_max = DEG2RAD(angle_max);
+    scan_msg.angle_min = radian_min;
+    scan_msg.angle_max = radian_max;
+    scan_msg.angle_increment = (scan_msg.angle_max - scan_msg.angle_min) / (double)(counts-1);
     scan_msg.scan_time = scan_time;
-    scan_msg.time_increment = scan_time / (double)(node_count-1);
-
+    scan_msg.time_increment = scan_time / (double)(counts-1);
     scan_msg.range_min = 0.08;
     scan_msg.range_max = 15.;
-
-    scan_msg.ranges.resize(node_count);
-    scan_msg.intensities.resize(node_count);
-    for (size_t i = 0; i < node_count; i++) {
-        scan_msg.ranges[i] = (float)nodes[i].distance_q2/4.0f/1000;
-        scan_msg.intensities[i] = (float) (nodes[i].sync_quality >> 2);
-    }
-
-    if(ignore_array.size() != 0)
-    {
-	for (size_t i = 0; i < node_count; i++) {
-            float angle = (float)((nodes[i].angle_q6_checkbit >> LIDAR_RESP_MEASUREMENT_ANGLE_SHIFT)/64.0f);
-            for(unsigned int j = 0; j < ignore_array.size();j = j+2){
-               if((ignore_array[j] < angle) && (angle <= ignore_array[j+1])){
-	           scan_msg.ranges[i] = ignore_value;
-                   break;
-               }
-            }
-        }
-    }
 
     pub->publish(scan_msg);
 }
 
-std::vector<int> split(const std::string &s, char delim) {
-    std::vector<int> elems;
-    std::stringstream ss(s);
-    std::string number;
-    while(std::getline(ss, number, delim)) {
-        elems.push_back(atoi(number.c_str()));
-    }
-    return elems;
-}
 
 int main(int argc, char * argv[]) {
     ros::init(argc, argv, "flash_lidar_node");
 
-    std::string serial_port;
-    int serial_baudrate;
+    std::string port;
+    int baudrate;
     std::string frame_id;
-    bool angle_compensate;
-
-    std::string list;
-    std::vector<int> ignore_array;
-    double ignore_value;
+    bool angle_fixed;
+    double angle_max,angle_min;
+    u_int32_t op_result;
 
     ros::NodeHandle nh;
     ros::Publisher scan_pub = nh.advertise<sensor_msgs::LaserScan>("scan", 1000);
     ros::NodeHandle nh_private("~");
-    nh_private.param<std::string>("serial_port", serial_port, "/dev/ttyACM0"); 
-    nh_private.param<int>("serial_baudrate", serial_baudrate, 115200); 
+    nh_private.param<std::string>("port", port, "/dev/ttyACM0"); 
+    nh_private.param<int>("baudrate", baudrate, 115200); 
     nh_private.param<std::string>("frame_id", frame_id, "laser_frame");
-    nh_private.param<bool>("angle_compensate", angle_compensate, "true");
-    nh_private.param<std::string>("ignore_array",list,"");
-    nh_private.param<double>("ignore_value", ignore_value, 0.1);
+    nh_private.param<bool>("angle_fixed", angle_fixed, "true");
+    nh_private.param<double>("angle_max", angle_max , 180);
+    nh_private.param<double>("angle_min", angle_min , -180);
 
-    ignore_array = split(list ,',');
-    if(ignore_array.size()%2){
-        ROS_ERROR_STREAM("ignore array is odd need be even");
-    }
-
-    for(unsigned int i =0 ; i < ignore_array.size();i++){
-        if(ignore_array[i] < 0 ||ignore_array[i] > 360){
-            ROS_ERROR_STREAM("ignore array should be 0<=  <=360");
-        }
-    }
-
-    u_int32_t op_result;
-
-    drv = Flashgo::initDriver();
-    
+    drv = Flashgo::initDriver(); 
     if (!drv) {
         fprintf(stderr, "Create Driver fail, exit\n");
         return -2;
     }
 
-    op_result = drv->connect(serial_port.c_str(), (u_int32_t)serial_baudrate);
+    op_result = drv->connect(port.c_str(), (u_int32_t)baudrate);
     if (op_result == -1) {
         int seconds=0;
         while(seconds <= DELAY_SECONDS){
             sleep(2);
             seconds = seconds + 2;
             drv->disconnect();
-            op_result = drv->connect(serial_port.c_str(), (u_int32_t)serial_baudrate);
-            fprintf(stdout, "EAI Info, try to connect the port %s again  after %d s .\n", serial_port.c_str() , seconds);
+            op_result = drv->connect(port.c_str(), (u_int32_t)baudrate);
+            fprintf(stdout, "EAI Info, try to connect the port %s again  after %d s .\n", port.c_str() , seconds);
         }
         
         if(seconds > DELAY_SECONDS){
-            fprintf(stderr, "EAI Error, cannot bind to the specified serial port %s.\n" , serial_port.c_str());
+            fprintf(stderr, "EAI Error, cannot bind to the specified serial port %s.\n" , port.c_str());
             return -1;
         }
     }
 
-    fprintf(stdout, "EAI Info, connected the port %s , start to scan ......\n", serial_port.c_str());
+    fprintf(stdout, "EAI Info, connected the port %s , start to scan ......\n", port.c_str());
     drv->startScan();
     fprintf(stdout, "EAI Info, Now Flash Lidar is scanning ......\n");
 
@@ -151,8 +116,7 @@ int main(int argc, char * argv[]) {
     double scan_duration;
     ros::Rate rate(60);
 
-    const int angle_compensate_nodes_count = NODE_COUNTS;
-    node_info angle_compensate_nodes[angle_compensate_nodes_count];
+    node_info all_nodes[NODE_COUNTS];
 
     while (ros::ok()) {
         node_info nodes[360*2];
@@ -165,11 +129,10 @@ int main(int argc, char * argv[]) {
 
         if (op_result == 0) {
             op_result = drv->ascendScanData(nodes, count);
-            float angle_min = DEG2RAD(0.0f);
-            float angle_max = DEG2RAD(360.0f);
+            
             if (op_result == 0) {
-                if (angle_compensate) {
-                    memset(angle_compensate_nodes, 0, angle_compensate_nodes_count*sizeof(node_info));
+                if (angle_fixed) {
+                    memset(all_nodes, 0, NODE_COUNTS*sizeof(node_info));
                     int i = 0 ;
                     for( ; i < count; i++) {
                         if (nodes[i].distance_q2 != 0) {
@@ -178,17 +141,13 @@ int main(int argc, char * argv[]) {
                             float angle_pre = angle - inter * EACH_ANGLE;
                             float angle_next = (inter+1) * EACH_ANGLE - angle;
                             if(angle_pre < angle_next){
-                                angle_compensate_nodes[inter]=nodes[i];
+                                all_nodes[inter]=nodes[i];
                             }else{
-                                angle_compensate_nodes[inter+1]=nodes[i];
+                                all_nodes[inter+1]=nodes[i];
                             }
                         }
                     }
-
-                    publish_scan(&scan_pub, angle_compensate_nodes, angle_compensate_nodes_count,
-                             start_scan_time, scan_duration,  
-                             angle_min, angle_max, 
-                             frame_id,ignore_array,ignore_value);
+                    publish_scan(&scan_pub, all_nodes, NODE_COUNTS, start_scan_time, scan_duration, angle_min, angle_max, frame_id);
                 } else {
                     int start_node = 0, end_node = 0;
                     int i = 0;
@@ -198,22 +157,11 @@ int main(int argc, char * argv[]) {
                     while (nodes[i--].distance_q2 == 0);
                     end_node = i+1;
 
-                    angle_min = DEG2RAD((float)(nodes[start_node].angle_q6_checkbit >> LIDAR_RESP_MEASUREMENT_ANGLE_SHIFT)/64.0f);
-                    angle_max = DEG2RAD((float)(nodes[end_node].angle_q6_checkbit >> LIDAR_RESP_MEASUREMENT_ANGLE_SHIFT)/64.0f);
+                    angle_min = (float)(nodes[start_node].angle_q6_checkbit >> LIDAR_RESP_MEASUREMENT_ANGLE_SHIFT)/64.0f;
+                    angle_max = (float)(nodes[end_node].angle_q6_checkbit >> LIDAR_RESP_MEASUREMENT_ANGLE_SHIFT)/64.0f;
 
-                    publish_scan(&scan_pub, &nodes[start_node], end_node-start_node +1, 
-                             start_scan_time, scan_duration, 
-                             angle_min, angle_max, 
-                             frame_id,ignore_array,ignore_value);
+                    publish_scan(&scan_pub, &nodes[start_node], end_node-start_node +1,  start_scan_time, scan_duration, angle_min, angle_max, frame_id);
                }
-            } else if (op_result == -2) {
-                float angle_min = DEG2RAD(0.0f);
-                float angle_max = DEG2RAD(360.0f);
-
-                publish_scan(&scan_pub, nodes, count, 
-                             start_scan_time, scan_duration,  
-                             angle_min, angle_max, 
-                             frame_id,ignore_array,ignore_value);
             }
         }
         rate.sleep();
